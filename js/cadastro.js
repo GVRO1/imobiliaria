cd4ac56754f60135b881ad4973d59f9cc1a4221e
GVRO1 <gabriel.oliveira@bandtec.com.br>
2019-10-18 21:23:15 -0300

    Cadatro atualizado

---
+++ js/cadastro.js
@@ -0,0 +1,7 @@
+function verifica_nome(){
+    var tamanho = nome_completo.lenght 
+    if(tamanho >= 5){
+     document.getElementById('nome_completo').className = 'nome_completo_verde';
+        alert("batat")
+    }
+}
