e8672026ec1b13a5fdf18716dbcd5fc9cf64114c
GVRO1 <gabriel.oliveira@bandtec.com.br>
2019-10-22 16:04:43 -0200

    Restrições da pagina cadastro criadas

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,35 +1,57 @@
+
+var senha_certa = 0;
 function verifica_nome(){
-    var tamanho = nome_completo.lenght 
+    var tamanho = nome_completo.value.length;
     if(tamanho >= 5){
-     document.getElementById('nome_completo').className = 'nome_completo_verde';
-        alert("batat")
+        if(!(nome_completo.value.startsWith(" ")) && !(nome_completo.value.endsWith(" ")) && nome_completo.value.indexOf(" ") > 0){        
+            document.getElementById('nome_completo').className = 'correto';
+        }
+        else{
+            document.getElementById('nome_completo').className = 'errado';
+        }
+    }
+    else{
+        document.getElementById('nome_completo').className = 'errado';
+    }
+}
+
+function verificar_email(){
+    if(email_cadastro.value.indexOf("@") > 0 && (email_cadastro.value.endsWith(".com") || email_cadastro.value.endsWith(".br"))){
+        document.getElementById('email_cadastro').className = 'correto';
+    }
+    else{
+        document.getElementById('email_cadastro').className = 'errado';
     }
 
-function verificar_nome(){
-    if (nome_completo.value.length > 5){
-        alert("teste");
-        nome_completo.style.border = '2px solid rgb(8, 177, 22)'
+}
+
+function verificar_senha(){
+    var tamanho = senha_cadastro.value.length;
+    if(tamanho > 5){
+        document.getElementById('senha_cadastro').className = 'correto';
+        senha_certa = 1;
+    }
+    else{
+        document.getElementById('senha_cadastro').className = 'errado';
+        senha_certa = 0 ;
+    }
+    if(senha_cadastro.value == senha_confirmar.value && senha_certa == 1){
+        document.getElementById('senha_confirmar').className = 'correto';
+    }
+    else{
+        document.getElementById('senha_confirmar').className = 'errado';
+
     }
 }
-function verificar_email(){
-    if(email_cadastro.value.indexOf("@") > 0 && (email_cadastro.value.endsWith(".com")||email_cadastro.value.endsWith(".com.br") ) ) {
-        alert("teste");
-        email_cadastro.style.border = '2px solid rgb(8, 177, 22)'
-    }
-}
+
 function function_mostrar_senha(){
     if(mostrar_senha.value = true){
         senha_cadastro.type = 'text';
-        mostrar_senha.onclick = 'function_esconder_senha';
+        senha_confirmar.type = 'text';
     }   
     else{
         senha_cadastro.type = 'password';
-    }
-}
-function function_esconder_senha(){
-    if(mostrar_senha.value = false){
-        senha_cadastro.type = 'password';
-        mostrar_senha.onclick = 'function_mostar_senha';
-    }
+        senha_confirmar.type = 'password';
 
+    }
 }
