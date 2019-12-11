33fa497521c424396e0a1aa28237f033d9fa6e94
GVRO1 <gabriel.oliveira@bandtec.com>
2019-12-11 16:46:21 -0200

    logica do filtro do caio funcionando

--- projeto-site/public/js/imoveis.js
+++ projeto-site/public/js/imoveis.js
@@ -1,46 +1,72 @@
-var endereco = [] ;
+var endereco = [];
 var nomeImovel = [];
 var preco = [];
-var descricao = [] ;
+var descricao = [];
 var tamanho = [];
-var tipo = [] ;
+var tipo = [];
 var negocio = [];
-if(sessionStorage.login_usuario_meuapp.length == 0){
+if (sessionStorage.login_usuario_meuapp.length == 0) {
     com_login.style.display = 'none';
-    }
-    else{
-        com_login.style.display = 'block';
-        var nomeSobrenome = sessionStorage.nome_usuario_meuapp.split(" ");
-        usuario.innerHTML = `${nomeSobrenome[0]} ${nomeSobrenome[nomeSobrenome.length - 1]}`;
-    }
-function fore(){
+}
+else {
+    com_login.style.display = 'block';
+    var nomeSobrenome = sessionStorage.nome_usuario_meuapp.split(" ");
+    usuario.innerHTML = `${nomeSobrenome[0]} ${nomeSobrenome[nomeSobrenome.length - 1]}`;
+}
+function fore() {
 
+    var busca = {
+        tipoImovel: subfiltro_tipo.value,
+        valor: subfiltro_valores.value,
+        localizacao: subfiltro_localizacao.value
+    };
 
+    // var meuJson = { variavel_1: 'xpto', variavel_2: 'abcd' };
 
+    fetch(`/leituras/busca/`, { 
+        cache: 'no-store',
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        // O "body" (corpo - em inglês) é responsável 
+        // por enviar todos os nossos parâmetros ao back-end
+        // para que possamos utilizá-los no servidor.
+        body: JSON.stringify(busca),
+    }).then(async function (response) {
+        // console.log('response', response)
+        if (response.ok) {
+            // console.log('response', response);
+            // console.log('JSON.parse(response)', JSON.parse(response));
+            // response.json().then(function (resposta) {
+                // console.log('resposta', resposta);
+                var resposta = await response.json();
+                if(resposta.length >= 1){
+                    resposta.reverse();
+                endereco = [];
+                nomeImovel = [];
+                preco = [];
+                descricao = [];
+                tamanho = [];
+                tipo = [];
+                negocio = [];
+                resultado.innerHTML = " "
+                for (i = 0; i < resposta.length; i++) {
+                    var registro = resposta[i];
+                    // aqui, após registro. use os nomes 
+                    // dos atributos que vem no JSON 
+                    // que gerou na consulta ao banco de dados
 
-fetch('/leituras/imoveis', {cache:'no-store'}).then(function (response) {
-    if (response.ok) {
-        response.json().then(function (resposta) {
-            
+                    endereco.push(registro.endereco);
+                    nomeImovel.push(registro.nomeImovel);
+                    preco.push(registro.preco);
+                    descricao.push(registro.descricao);
+                    tamanho.push(registro.tamanho);
+                    tipo.push(registro.tipo);
+                    negocio.push(registro.negocio);
 
-            resposta.reverse();
-            
-            resultado.innerHTML = " "
-            for (i = 0; i < resposta.length; i++) {
-                var registro = resposta[i];
-                // aqui, após registro. use os nomes 
-                // dos atributos que vem no JSON 
-                // que gerou na consulta ao banco de dados
-                
-                endereco.push(registro.endereco);
-                nomeImovel.push(registro.nomeImovel);
-                preco.push(registro.preco);
-                descricao.push(registro.descricao);
-                tamanho.push(registro.tamanho);
-                tipo.push(registro.tipo);
-                negocio.push(registro.negocio);
-            
-                resultado.innerHTML += `
+                    resultado.innerHTML += `
                 <div class="imovel">
                             <img class="foto" src="../fotos/imovel1.jpeg" alt="">
                         <span class="preco">R$${preco[i]},00</span> <span class="mes">/mês</span>
@@ -52,15 +78,18 @@ fetch('/leituras/imoveis', {cache:'no-store'}).then(function (response) {
 
                     </div>
                                         `;
-            }
+                    }
+                }else{
+                    alert('Não há resultados ...');
+                }
 
+            // });
+        } else {
+            console.error('Nenhum dado encontrado ou erro na API');
+        }
+    })
+        .catch(function (error) {
+            console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
         });
-    } else {
-        console.error('Nenhum dado encontrado ou erro na API');
-    }
-})
-.catch(function (error) {
-    console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
-});
 
 }
--- projeto-site/routes/leituras.js
+++ projeto-site/routes/leituras.js
@@ -20,6 +20,54 @@ router.get('/imoveis', function(req, res, next) {
 			res.status(500).send(erro.message);
 	  });
 });
+router.get('/imoveis/Comercial', function(req, res, next) {
+	
+	const instrucaoSql = `select * from imoveis where tipo = 'Comercial' order by idImovel desc`;
+
+	sequelize.query(instrucaoSql, {
+		model: imoveis,
+		mapToModel: true 
+	  })
+	  .then(resultado => {
+			console.log(`Encontrados: ${resultado.length}`);
+			res.json(resultado);
+	  }).catch(erro => {
+			console.error(erro);
+			res.status(500).send(erro.message);
+	  });
+});
+router.get('/imoveis/Residencial', function(req, res, next) {
+	
+	const instrucaoSql = `select * from imoveis where tipo = 'Residencial' order by idImovel desc`;
+
+	sequelize.query(instrucaoSql, {
+		model: imoveis,
+		mapToModel: true 
+	  })
+	  .then(resultado => {
+			console.log(`Encontrados: ${resultado.length}`);
+			res.json(resultado);
+	  }).catch(erro => {
+			console.error(erro);
+			res.status(500).send(erro.message);
+	  });
+});
+router.get('/imoveis/Terreno', function(req, res, next) {
+	
+	const instrucaoSql = `select * from imoveis where tipo = 'Terreno' order by idImovel desc`;
+
+	sequelize.query(instrucaoSql, {
+		model: imoveis,
+		mapToModel: true 
+	  })
+	  .then(resultado => {
+			console.log(`Encontrados: ${resultado.length}`);
+			res.json(resultado);
+	  }).catch(erro => {
+			console.error(erro);
+			res.status(500).send(erro.message);
+	  });
+});
 
 
 // tempo real (último valor de cada leitura)
@@ -39,6 +87,36 @@ router.get('/tempo-real', function (req, res, next) {
   
 });
 
+router.post('/busca', function(req, res, next) {
+	
+	// console.log('R E Q: ', req.body);
+	var dados = req.body;
+	var precoQuery = '';
+	if(dados.valor <= 500){
+		precoQuery = 'AND preco BETWEEN 0 AND 500';
+	}else if(dados.valor <= 1000){
+		precoQuery = 'AND preco BETWEEN 501 AND 1000';
+	}else if(dados.valor <= 1500){
+		precoQuery = 'AND preco BETWEEN 1001 AND 1500';
+	}else{
+		precoQuery = 'AND preco > 1500';
+	}
+	var instrucaoSql = `SELECT * FROM imoveis 
+						WHERE endereco LIKE '%${dados.localizacao}%'
+						AND tipo = '${dados.tipoImovel}'
+						${precoQuery}`;
+
+	// Aqui nós nos conectamos ao banco e enviamos uma Query (busca) á ele e
+	// esperamos o retorno disso.
+	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
+		.then(resultado => {
+			res.send(resultado);
+		}).catch(erro => {
+			console.error(erro);
+			res.status(500).send(erro.message);
+		});
+
+});
 
 // estatísticas (max, min, média, mediana, quartis etc)
 router.get('/estatisticas', function (req, res, next) {
