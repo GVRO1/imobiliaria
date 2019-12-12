3aca021b11aa4d9b61fd639a632583fad5f545e9
GVRO1 <gabriel.oliveira@bandtec.com.br>
2019-12-12 11:30:34 -0300

    Filtro iniciado a alteração

--- projeto-site/public/js/imoveis.js
+++ projeto-site/public/js/imoveis.js
@@ -13,6 +13,17 @@ else {
     var nomeSobrenome = sessionStorage.nome_usuario_meuapp.split(" ");
     usuario.innerHTML = `${nomeSobrenome[0]} ${nomeSobrenome[nomeSobrenome.length - 1]}`;
 }
+function btn_comprar(){
+    id_btn_comprar.classList = "btn_comprar_selecionado";
+    id_btn_alugar.classList.remove("btn_alugar_selecionado");
+
+    
+}
+function btn_alugar(){
+    id_btn_alugar.classList = "btn_alugar_selecionado";
+    id_btn_comprar.classList.remove("btn_comprar_selecionado");
+
+}
 function fore() {
 
     var busca = {
